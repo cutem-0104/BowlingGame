4e39834053005f073cdef998ac060c5a5b3ff88f
cutem-0104 <shuhei.sugaya@gmail.com>
2019-06-26 00:07:38 +0900

    Frameクラス作成

---
+++ src/main/java/Frame.java
@@ -0,0 +1,7 @@
+// Frame.java------------------
+public class Frame {
+
+  public int getScore() {
+    return 0;
+  }
+}
---
+++ src/test/java/TestFrame.java
@@ -0,0 +1,15 @@
+// TestFrame.java------------------
+
+import junit.framework.*;
+
+public class TestFrame extends TestCase {
+
+  public TestFrame(String name) {
+    super(name);
+  }
+
+  public void testScoreNoThrows() {
+    Frame f = new Frame();
+    assertEquals(0, f.getScore());
+  }
+}
