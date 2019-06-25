93ab13aa5f584fd6a827771ae3cee3ab86770f86
cutem-0104 <shuhei.sugaya@gmail.com>
2019-06-26 00:49:56 +0900

    Gameクラス作成

---
+++ src/main/java/Game.java
@@ -0,0 +1,15 @@
+// Game.java------------------------
+
+public class Game {
+
+  public int score() {
+    return itsScore;
+  }
+
+  public void add(int pins) {
+    itsScore += pins;
+  }
+
+  private int itsScore = 0;
+
+}
