0de7f4eb45c5b8d454a24196469b82a023225e19
cutem-0104 <shuhei.sugaya@gmail.com>
2019-06-28 01:26:46 +0900

    getCurrentFrameリファクタリング（テスト成功）

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -6,9 +6,19 @@ public class Game {
     return itsScore;
   }
 
+  public int getCurrentFrame() {
+    return itsCurrentFrame;
+  }
+
   public void add(int pins) {
     itsThrows[itsCurrentThrow++] = pins;
     itsScore += pins;
+    if (firstThrow) {
+      firstThrow = false;
+      itsCurrentFrame++;
+    } else {
+      firstThrow = true;
+    }
   }
 
   public int scoreForFrame(int theFrame) {
@@ -29,12 +39,10 @@ public class Game {
     return score;
   }
 
-  public int getCurrentFrame() {
-    return 1 + (itsCurrentThrow - 1) / 2;
-  }
-
   private int itsScore = 0;
   private int[] itsThrows = new int[21];
   private int itsCurrentThrow = 0;
+  private int itsCurrentFrame = 0;
+  private boolean firstThrow = true;
 
 }
