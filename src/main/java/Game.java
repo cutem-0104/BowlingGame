c8140714568552548e79f54a6bdd209d2d7710ca
cutem-0104 <shuhei.sugaya@gmail.com>
2019-07-02 01:08:50 +0900

    adjustFrameForStrikeメソッド定義（テスト成功）

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -16,15 +16,21 @@ public class Game {
 
   private void adjustCurrentFrame(int pins) {
     if (firstThrowInFrame) {
-      if (pins == 10) { // ストライク
-        advanceFrame();
-      } else {
+      if (!adjustFrameForStrike(pins)) { // ストライク
         firstThrowInFrame = false;
       }
     } else {
       firstThrowInFrame = true;
       advanceFrame();
     }
+  }
+
+  private boolean adjustFrameForStrike(int pins) {
+    if (pins == 10) {
+      advanceFrame();
+      return true;
+    }
+    return false;
   }
 
   private void advanceFrame() {
